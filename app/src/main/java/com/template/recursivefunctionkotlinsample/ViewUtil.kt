c5d97be7c2836d6509e4abb63fce0096f6818a1c
Reo Ando <ando.reo420@gmail.com>
2021-02-26 03:13:59 +0900

    rename

--- app/src/main/java/com/template/recursivefunctionkotlinsample/ViewUtil.kt
+++ app/src/main/java/com/template/recursivefunctionkotlinsample/ViewUtil.kt
@@ -5,13 +5,13 @@ import android.view.ViewGroup
 import android.widget.ToggleButton
 
 object ViewUtil {
-    private fun getViewsTree(view: View?): List<View> {
+    private fun getViewTree(view: View?): List<View> {
         val views = mutableListOf<View>()
         if (view is ViewGroup) {
             var count = view.childCount
             while (0 <= count) {
                 val child = view.getChildAt(count - 1)
-                views.addAll(getViewsTree(child))
+                views.addAll(getViewTree(child))
                 count--
             }
         }
@@ -20,7 +20,7 @@ object ViewUtil {
     }
 
     fun setOnClickListenerForToggleButton(vg: View, l: View.OnClickListener?) {
-        val viewTree = getViewsTree(vg)
+        val viewTree = getViewTree(vg)
         viewTree.filterIsInstance<ToggleButton>().forEach { toggleButton ->
             toggleButton.setOnClickListener(l)
         }
